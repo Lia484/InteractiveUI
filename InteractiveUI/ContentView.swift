0e59e03f3a0a1e6a6e913048c89d0f92b5d97fae
Lia <lworland28@stjoesphacademy.org>
2025-06-04 14:46:04 -0500

    added name, text box, and button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
+        VStack {
+            Text("What is your name?")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple)
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font(.largeTitle)
+                .border(Color.purple, width: 3)
+            Button("Submit") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }//end button
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }// end vstack
+        .padding()
     }//body end
 }//struct end
 
